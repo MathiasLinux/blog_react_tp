f395656b16998851624ef6025d36f41ec0c56c7e
mathiaskliem <kliem.mathias@gmail.com>
2023-09-16 21:25:37 +0200

    Ajout de case qui s'ouvre pour les questions

--- src/Componants/Question.js
+++ src/Componants/Question.js
@@ -2,15 +2,26 @@ import React from 'react';
 import "../Styles/question.css";
 
 function Question({question, answer}) {
-    function openMenuClick() {
-        
+
+    const openMenuClick = (e) => {
+        let target = e.target
+        let svgArrow = target.querySelector(".svgDefaultArrow")
+        if (target.classList.contains("menuOpenQuestion")) {
+            target.classList.remove("menuOpenQuestion");
+            svgArrow.classList.remove("svgRotateArrow")
+        } else {
+            target.classList.add("menuOpenQuestion")
+            svgArrow.classList.add("svgRotateArrow")
+        }
+
     }
 
     return (
         <div className="divQuestion">
-            <div className="theQuestion" onClick={openMenuClick}>
+            <div className="theQuestion" onClick={((e) => openMenuClick(e))}>
                 <h3>{question}</h3>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                <svg className="svgDefaultArrow" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                     viewBox="0 0 24 24" fill="none"
                      stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                     <path d="M6 9l6 6 6-6"/>
                 </svg>
--- src/Pages/Faq.js
+++ src/Pages/Faq.js
@@ -8,7 +8,9 @@ const Faq = () => {
         <div>
             <Header title="Foire aux questions" description="Les questions les plus possées sont disponible plus bas."
                     url={bgFaq}/>
-            <Question question="Question 1"
+            <Question question="Question 1 ?"
+                      answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu tellus a nunc volutpat eleifend in ac quam. Phasellus eu tortor vitae nibh semper convallis id ut felis. Integer in turpis et elit pharetra commodo. Duis vehicula vehicula malesuada. Nulla a ligula sit amet lorem tincidunt auctor nec nec odio. Quisque quis est pellentesque, pretium est in, vulputate odio. Integer porta auctor metus eu luctus. Integer lobortis libero quis turpis dapibus, quis auctor diam vulputate. Pellentesque mi metus, consectetur vitae erat a, pharetra iaculis lacus. Suspendisse potenti. Nam finibus magna a lectus aliquet, vitae pharetra quam mollis. In commodo metus a risus sagittis commodo. Fusce luctus at sem suscipit lacinia. Pellentesque imperdiet ante nec quam aliquam, a auctor quam sagittis. Etiam in sem iaculis, pellentesque turpis eu, posuere massa."/>
+            <Question question="Question 2 ?"
                       answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eu tellus a nunc volutpat eleifend in ac quam. Phasellus eu tortor vitae nibh semper convallis id ut felis. Integer in turpis et elit pharetra commodo. Duis vehicula vehicula malesuada. Nulla a ligula sit amet lorem tincidunt auctor nec nec odio. Quisque quis est pellentesque, pretium est in, vulputate odio. Integer porta auctor metus eu luctus. Integer lobortis libero quis turpis dapibus, quis auctor diam vulputate. Pellentesque mi metus, consectetur vitae erat a, pharetra iaculis lacus. Suspendisse potenti. Nam finibus magna a lectus aliquet, vitae pharetra quam mollis. In commodo metus a risus sagittis commodo. Fusce luctus at sem suscipit lacinia. Pellentesque imperdiet ante nec quam aliquam, a auctor quam sagittis. Etiam in sem iaculis, pellentesque turpis eu, posuere massa."/>
         </div>
     );
